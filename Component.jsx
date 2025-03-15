7f9f9697494851d23650ac919d98852735d0286c
minionAxlight <gjuanmanuel342@gmail.com>
2025-03-14 20:00:11 -0600

    Add files via upload

---
+++ Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Component() {
+  return (
+    <div>Que quieres aprender sobre ensamblador?
+
+    <input type = "text"/>
+    <button>buscar?</button>
+    <p>para que es?</p>
+    <p>Aplicaciones</p>
+    </div>
+  )
+}
